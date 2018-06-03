75f2db12598085fc816b3c7b473f7d81e697c1d2
jancuz <jancuz@yandex.ru>
2018-06-03 14:38:01 +0500

    Исправлено условие определения порядка матрицы, добавлены комментарии

--- ex5/Program.cs
+++ ex5/Program.cs
@@ -10,50 +10,52 @@ namespace ex5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите матрицу построчно через пробелы: \n");
+            Console.WriteLine("Введите матрицу построчно через пробелы (целые числа): \n");
             string str = Console.ReadLine();
 
             int count = str.Trim().Split(' ').Length;  // по первой строке инициализируем порядок матрицы
-            int[,] matrix = new int[count, count];             // инициализируем матрицу по полученным данным
+            int[,] matrix = new int[count, count];     // инициализируем матрицу по полученным данным
 
             try
-            {                                           // расщифровки       
+            {                                       // расщифровки       
                 var nums = str.Trim().Split(' ');   // первая заполненная строка
                 for (int i = 0; i < count; i++)
                 {
                     if (!int.TryParse(nums[i], out matrix[0,i]))
-                        throw new AggregateException("Неверно введено число");
+                        throw new AggregateException("Неверно введена строка матрицы");
                 }
 
-                for (int i = 1; i < count; i++)         // дешифровка всех оставшихся строк
+                for (int i = 1; i < count; i++)     // дешифровка всех оставшихся строк
                 {
                     str = Console.ReadLine();
                     nums = str.Trim().Split(' ');
-                    if (nums.Length != count)                   // если недостаточно, то
-                        throw new AggregateException("Недостаточно цифр в строке матрицы");
+                    if (nums.Length != count)
+                        throw new AggregateException("Недостаток/избыток чисел в строке матрицы");
                     for (int j = 0; j < count; j++)
                     {
                         if (!int.TryParse(nums[j], out matrix[i, j]))
-                            throw new AggregateException("Неверно введено число");
+                            throw new AggregateException("Неверно введена строка матрицы");
                     }
                 }
             }
             catch (AggregateException e)
             {
-                Console.WriteLine(e.Message);           // если встречена ошибка, то выписать ее
+                Console.WriteLine(e.Message);       // неправильный ввод
                 return;
             }
 
             int min = int.MaxValue;
 
-            int pos1 = count / 2;
-            int pos2 = pos1;
-            int pos3;
-            if (count / 2 != 0)
-                pos3 = pos1;
+            int pos1 = count / 2;   // строка - вершина треугольника
+            int pos2 = pos1;        // столбец - вершина треугольника (левая граница)
+            int pos3;               // столбец - вершина треугольника (правая граница)
+            if (count % 2 != 0)     // если порядок матрицы нечетный
+                pos3 = pos1;        // вершина треугольника одна
             else
-                pos3 = pos1 + 1;
-
+                pos3 = pos1 + 1;    // две вершины при четном порядке матрицы
+            
+            // проход по нижнему треугольнику матрицы
+            // поиск минимального элемента
             for (int i = pos1; i < count; i++)
                 {
                     for (int j = pos2; j <= pos3; j++)
@@ -65,7 +67,8 @@ namespace ex5
                     pos3++;
                 }
 
-            Console.WriteLine(min);
+            Console.WriteLine("Минимальный элемент в нижнем треугольнике матрицы: {0}", min);
+            Console.ReadLine();
         }
     }
 }
